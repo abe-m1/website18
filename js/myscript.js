7a1f3b6a7252dd71a8b7cb338ee0ea4c7dfe9181
Abe Menashy <abestbet1@gmail.com>
2016-01-07 12:11:51 -0800

    fixed mobile navbar added some tablet styling

--- js/myscript.js
+++ js/myscript.js
@@ -81,7 +81,11 @@ $(function() {
   });
 });
 
-
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+});
 
 });
 
